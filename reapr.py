9319f0bd9c5ef7e96517de66024cfe32a8daad14
RK Davies <rkdavies@RKs-MacBook-Pro.attlocal.net>
2022-11-26 01:40:10 -0800

    Catching timeouts

--- reapr.py
+++ reapr.py
@@ -7,6 +7,7 @@
 #      watching the Skinwalker Insiders stream.
 
 import sys
+import time
 import pytchat
 import logging
 
@@ -15,7 +16,7 @@ log = logging.getLogger()
 
 print("Starting REAPR - Reporting Events, Anomalous Phenomena and Requests")
 YouTube_ID = " ".join(sys.argv[1:])
-def read_chat():
+def read_chat(YouTube_ID):
     chat = pytchat.create(video_id=YouTube_ID)
     while chat.is_alive():
         for c in chat.get().sync_items():
@@ -25,27 +26,31 @@ def read_chat():
             if c.message.startswith(('#EVENT:')):
                 # Ideally this would get logged to a database
                 with open('events.txt', 'a') as f:
-                    f.write(f"\nEVENT: {c.datetime} [{c.author.name}]- {c.message}")
+                    f.write(f"EVENT: {c.datetime} [{c.author.name}]- {c.message}\n")
                 print(f"EVENT: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#REQUEST:')):
                 # Ideally this would get logged to a database
                 with open('requests.txt', 'a') as f:
-                    f.write(f"\nREQUEST:: {c.datetime} [{c.author.name}]- {c.message}")
+                    f.write(f"REQUEST: {c.datetime} [{c.author.name}]- {c.message}\n")
                 print(f"REQUEST: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#THOUGHT:')):
                 # Ideally this would get logged to a database
                 with open('thoughts.txt', 'a') as f:
-                    f.write(f"\nTHOUGHT: {c.datetime} [{c.author.name}]- {c.message}")
+                    f.write(f"THOUGHT: {c.datetime} [{c.author.name}]- {c.message}\n")
                 print(f"THOUGHT: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#ALERT:')):
                 # Ideally this would trigger an email or message to someone.
                 with open('alerts.txt', 'a') as f:
-                    f.write(f"\nTHOUGHT: {c.datetime} [{c.author.name}]- {c.message}")
-                print(f"THOUGHT: {c.datetime} [{c.author.name}] {c.message}")
-            
+                    f.write(f"ALERT: {c.datetime} [{c.author.name}]- {c.message}\n")
+                print(f"ALERT: {c.datetime} [{c.author.name}] {c.message}")
+
 def main(YouTube_ID):
     try:
-        read_chat()
+        read_chat(YouTube_ID)
+        sys.exit(1)
     except:
+        print("*** TIMEOUT ***")
+        time.sleep(1)
+        read_chat(YouTube_ID)
         pass
 main(YouTube_ID)
