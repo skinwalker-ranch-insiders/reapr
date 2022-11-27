bfbd7dd6590d785336de0778667c20162e05d95f
Robert Davies <robert.kris.davies@gmail.com>
2022-11-27 00:24:37 -0800

    Giving REAPR his cloak.

---
+++ rcloak.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+while true
+do
+   pgrep -f 'python3 ./reapr'>/dev/null
+   if [[ $? -ne 0 ]] ; then
+        echo "Restarting REAPR:     $(date)" >> ./reapr_restarts.txt
+        /usr/local/bin/python3 ./reapr.py $1
+   fi
+done
