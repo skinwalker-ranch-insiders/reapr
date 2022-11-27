b8390687355b85607c64022b95e9e99cf0b0d9d4
Robert Davies <robert.kris.davies@gmail.com>
2022-11-26 23:58:24 -0800

    Databasified!

--- reapr.py
+++ reapr.py
@@ -11,38 +11,72 @@ import time
 import pytchat
 import logging
 
+print("Starting REAPR - Reporting Events, Anomalous Phenomena and Requests")
+
 logging.basicConfig(level=logging.ERROR)
 log = logging.getLogger()
-
-print("Starting REAPR - Reporting Events, Anomalous Phenomena and Requests")
 YouTube_ID = " ".join(sys.argv[1:])
+
+def SWR_YT_MSG(YT_Tag, YT_DateTime, YT_User, YT_Msg):
+    # Contributed by johns67567
+    # depends on what DB is being used.
+    import pyodbc
+    from settings import db_server, db_user, db_passwd, db_name
+
+    # Used for MySQL commands and connections
+    import pymysql
+
+    conn = pymysql.connect(host=db_server,user=db_user,passwd=db_passwd,db=db_name)
+    cursor = conn.cursor()
+    SWR_YT_Entry = "INSERT INTO yt_events (YT_Tag, YT_DateTime, YT_User, YT_msg) VALUES (%s, %s, %s, %s)"
+
+    cursor.execute(SWR_YT_Entry,(YT_Tag, YT_DateTime, YT_User, YT_Msg))
+    conn.commit()
+
+def map_tags_dict(test_case: str = None):
+    tag_types = {
+    '#EVENT:': event_function,
+    '#THOUGHT:': thought_function,
+    '#REQUEST:': request_function,
+    '#ALERT:': alert_function,
+    }
+
+    # Call the mapping function based on test_case as key
+    tag_types[tag_type]()
+
 def read_chat(YouTube_ID):
     chat = pytchat.create(video_id=YouTube_ID)
     while chat.is_alive():
         for c in chat.get().sync_items():
             # Lets read all chat if we set logging to INFO
             logging.info(f"{c.datetime} [{c.author.name}]- {c.message}")
+            YT_DateTime=c.datetime
+            YT_User=c.author.name
+            YT_Msg=c.message
             # See tag send it somewhere. TXT for now, DB is the dream
             if c.message.startswith(('#EVENT:')):
                 # Ideally this would get logged to a database
-                with open('events.txt', 'a') as f:
-                    f.write(f"EVENT: {c.datetime} [{c.author.name}]- {c.message}\n")
+                YT_Tag='EVENT'
+                SWR_YT_MSG(YT_Tag, YT_DateTime, YT_User, YT_Msg)
                 print(f"EVENT: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#REQUEST:')):
                 # Ideally this would get logged to a database
-                with open('requests.txt', 'a') as f:
-                    f.write(f"REQUEST: {c.datetime} [{c.author.name}]- {c.message}\n")
+                YT_Tag='REQUEST'
+                SWR_YT_MSG(YT_Tag, YT_DateTime, YT_User, YT_Msg)
                 print(f"REQUEST: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#THOUGHT:')):
                 # Ideally this would get logged to a database
-                with open('thoughts.txt', 'a') as f:
-                    f.write(f"THOUGHT: {c.datetime} [{c.author.name}]- {c.message}\n")
+                YT_Tag='THOUGHT'
+                SWR_YT_MSG(YT_Tag, YT_DateTime, YT_User, YT_Msg)
                 print(f"THOUGHT: {c.datetime} [{c.author.name}] {c.message}")
             elif c.message.startswith(('#ALERT:')):
                 # Ideally this would trigger an email or message to someone.
-                with open('alerts.txt', 'a') as f:
-                    f.write(f"ALERT: {c.datetime} [{c.author.name}]- {c.message}\n")
+                YT_Tag='ALERT'
+                SWR_YT_MSG(YT_Tag, YT_DateTime, YT_User, YT_Msg)
                 print(f"ALERT: {c.datetime} [{c.author.name}] {c.message}")
+            elif not chat.is_alive:
+                print("NOT is_alive caught.")
+                main(YouTube_ID)
 
 def main(YouTube_ID):
     try:
@@ -53,4 +87,5 @@ def main(YouTube_ID):
         time.sleep(1)
         read_chat(YouTube_ID)
         pass
+
 main(YouTube_ID)
