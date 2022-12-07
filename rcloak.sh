98b1545b7578941158623e5e78d0f1df121e6efe
RK Davies <rkdavies@RKs-MacBook-Pro.local>
2022-12-06 20:31:41 -0800

    Fixing rcloak

--- rcloak.sh
+++ rcloak.sh
@@ -4,6 +4,6 @@ do
    pgrep -f 'python3 ./reapr'>/dev/null
    if [[ $? -ne 0 ]] ; then
         echo "Restarting REAPR:     $(date)" >> ./reapr_restarts.txt
-        /usr/local/bin/python3 ./reapr.py $1
+        /usr/local/bin/python3 ./reapr.py
    fi
 done
